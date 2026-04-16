4924f0cf2ff817a9927cf9185503331927037a0d
devil233 <1250226509@qq.com>
2026-04-17 02:33:22 +0800

    ⚡️ 崩铁pv都能有横幅到期还行，进行一个开除

--- plugins/genshin/model/mysNews.js
+++ plugins/genshin/model/mysNews.js
@@ -509,7 +509,7 @@ export default class MysNews extends base {
         let hdlist = []
         let result = []
         for (let item of srhd.data.pic_list[0].type_list[0].list) {
-            if (item.title.includes("活动") || item.type_label.includes("资讯") && !item.title.includes("签到")) hdlist.push(item)
+            if (item.title.includes("活动") || item.type_label.includes("资讯") && !item.title.includes("签到") || item.type_label.includes("资讯") && !item.title.includes("PV")) hdlist.push(item)
         }
         for (let item of hdlist) {
             let endDt = item.end_time
